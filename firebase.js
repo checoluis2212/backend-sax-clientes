2b5487fbc268a251b19b99a3c2740bc8077fab2a
checoluis2212 <luissergio.mkt@gmail.com>
2025-07-23 13:23:31 -0600

    Update firebase.js
    Agrega storageBucket en initializeApp para la carga de archivos CV

--- firebase.js
+++ firebase.js
@@ -6,7 +6,10 @@ admin.initializeApp({
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   }),
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // 🔥 Agrega esto
 });
 
 const db = admin.firestore();
-module.exports = db;
+const bucket = admin.storage().bucket(); // 🔥 Esto te permite subir archivos
+
+module.exports = { db, bucket }; // 🔥 Exporta ambos
