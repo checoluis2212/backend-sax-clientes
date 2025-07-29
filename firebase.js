0b868be642edba0bdf046f46c0388b61be879488
checoluis2212 <luissergio.mkt@gmail.com>
2025-07-29 17:50:07 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,14 +2,14 @@ const admin = require('firebase-admin');
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    projectId:        process.env.FIREBASE_PROJECT_ID,
+    privateKey:       process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail:      process.env.FIREBASE_CLIENT_EMAIL,
   }),
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // 🔥 Agrega esto
+  storageBucket:     process.env.FIREBASE_STORAGE_BUCKET,
 });
 
-const db = admin.firestore();
-const bucket = admin.storage().bucket(); // 🔥 Esto te permite subir archivos
+const db     = admin.firestore();
+const bucket = admin.storage().bucket();
 
-module.exports = { db, bucket }; // 🔥 Exporta ambos
+module.exports = { admin, db, bucket };
