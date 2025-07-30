7ac460fc7bd303752e3b271230d8290909128856
checoluis2212 <luissergio.mkt@gmail.com>
2025-07-29 20:22:47 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,14 @@
+// src/firebase.js
+require('dotenv').config();
 const admin = require('firebase-admin');
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    projectId:        process.env.FIREBASE_PROJECT_ID,
-    privateKey:       process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    clientEmail:      process.env.FIREBASE_CLIENT_EMAIL,
+    projectId:   process.env.FIREBASE_PROJECT_ID,
+    privateKey:  process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   }),
-  storageBucket:     process.env.FIREBASE_STORAGE_BUCKET,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 });
 
 const db     = admin.firestore();
